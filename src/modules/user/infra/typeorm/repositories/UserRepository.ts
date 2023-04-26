756c0e26c1f7471100d7cef371edf421110fb6f7
Igor Emerick <igorbarbosaemerick@gmail.com>
2023-04-25 23:20:29 -0300

    feature: create user repository

---
+++ src/modules/user/infra/typeorm/repositories/UserRepository.ts
@@ -0,0 +1,20 @@
+import { DataSource, Repository } from 'typeorm';
+import { IUserRepository } from '@modules/user/repositories/IUserRepository';
+import { ICreateUserDTO } from '@modules/user/dtos/ICreateUserDTO';
+import { User } from '../entities/User';
+
+export class UserRepository implements IUserRepository {
+  private static repository: Repository<User>;
+
+  constructor(data_source: DataSource) {
+    if (!UserRepository.repository) {
+      UserRepository.repository = data_source.getRepository(User);
+    }
+  }
+
+  async create(user: ICreateUserDTO): Promise<User> {
+    const newUser = UserRepository.repository.create(user);
+
+    return UserRepository.repository.save(newUser);
+  }
+}
